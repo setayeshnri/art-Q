6e737850a3379db00c02a39d83b4dfaffeffe9af
setayeshnri <setayeshn1382@gmail.com>
2023-12-05 17:29:32 +0100

    edited

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const startButton = document.querySelector("#logo_btn");
-const popup = document.querySelector("#info");
-const closePopupButton = document.querySelector("#close_btn");
-startButton.onclick = function () {
-  popup.style.display = "block";
-};
-closePopupButton.onclick = function () {
-  popup.style.display = "none";
-};
+const closePopupBtn = document.querySelector("#close_btn");
 
-window.onclick = function (event) {
-  if (event.target == popup) {
-    popup.style.display = "none";
+const startButton = document.querySelector("#button-start");
+const popup = document.querySelector(".popup");
+const closePopupButton = document.querySelector(".popup__button-close");
+const addNameForm = document.querySelector(".popup__form");
+const sendNameButton = document.querySelector("#button-send-name");
+const nameInput = document.querySelector("#input-name");
+
+// Open and close popup:
+function openPopup() {
+  popup.classList.add("popup_is-opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_is-opened");
+}
+
+startButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+
+// Form "add name"
+function setPopupButtonState(isValid) {
+  if (isValid) {
+    sendNameButton.removeAttribute("disabled");
+    sendNameButton.classList.remove("button_disabled");
+  } else {
+    sendNameButton.setAttribute("disabled", true);
+    sendNameButton.classList.add("button_disabled");
   }
+}
+addNameForm.addEventListener("input", function () {
+  const isValid = nameInput.value.length > 0;
+
+  setPopupButtonState(isValid);
+});
+addNameForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  localStorage.setItem("playerName", JSON.stringify(nameInput.value));
+
+  setPopupButtonState(false);
+  addNameForm.reset();
+  closePopup();
+
+  // Redirect to cards page
+  window.location.href = "cards.html";
+});
+closePopupBtn.onclick = function () {
+  popup.style.display = "none";
 };
